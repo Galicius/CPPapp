489443859ff00fd1d3b125bea357da62a8ed9c22
GuštinGal <70420472+Galicius@users.noreply.github.com>
2025-10-15 00:52:09 +0200

    send email set to resend testing one

--- backend/notifier.py
+++
@@ -1,4 +0,0 @@
-# notifier.py
-def notify_user(user, slot):
-    # TODO: replace with Postmark/Twilio
-    print(f"[notify][{user['channel']}] -> {user['address']} : {slot['date_str']} {slot['time_str']} | {slot['location']} | {slot['categories']}")
